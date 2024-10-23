8e7f1de0de95e895e10a916fae17a7b70393532a
Olga-Lashko <olgamordacheva@gmail.com>
2024-10-22 19:53:37 -0500

    Add project files.

---
+++ XEx09Reservation/Site.Master.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace XEx09Reservation
+{
+    public partial class Site : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Sets the text of the label in the footer to the current year
+            lblYear.Text = DateTime.Today.Year.ToString();
+        }
+    }
+}
